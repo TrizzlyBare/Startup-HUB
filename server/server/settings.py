db1567a70ecff37dd008f38caa14f0ec87f61053
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2025-02-18 15:25:21 +0700

    docker

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = "django-insecure-86$g$4gj_g$7kqd7vmr5n-r&v-m62qtaofqf_t^**k$@24a!1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
