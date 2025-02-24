af261dc4e1516042ec18e6c654524945be4bda19
UncleCM <earth.thuwanon@gmail.com>
2025-02-24 18:50:20 +0700

    Reapply "change to .env"
    This reverts commit 3ded6276dfcad2137013ca2d73b486f834d350c7.

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,10 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = rx.Config(
     app_name="Startup_HUB",
+    api_url=os.getenv("API_URL", "http://localhost:8000"),
 )
