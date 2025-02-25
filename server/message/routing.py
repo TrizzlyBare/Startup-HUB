6c32e54801df2d93fe4b6afc18e29e3bd69d65a3
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2025-02-25 21:07:37 +0700

    fix

--- server/message/routing.py
+++ server/message/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_id>[0-9a-f-]+)/$", consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/echo/$', consumers.EchoConsumer.as_asgi()),
+    re_path(r"^ws/echo/?$", consumers.EchoConsumer.as_asgi()),
 ]
