b5d51988decfbc180fb2f68f5e04b12643ccaf9c
UncleCM <earth.thuwanon@gmail.com>
2025-04-02 03:27:01 +0700

    it fucking work now fuck you everyone I am now god

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 import reflex as rx
+
+# Load environment variables from .env file
+load_dotenv()
 
 config = rx.Config(
     app_name="Startup_HUB",
+    server_url=os.getenv("SERVER_URL")  # Use the environment variable
 )
