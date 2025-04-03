2a782dc450b3fd52ca66deee7e1e12519032b2f1
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2025-04-03 23:25:22 +0700

    Redis

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -182,7 +183,12 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [
+                (
+                    os.environ.get("REDIS_HOST", "redis"),
+                    int(os.environ.get("REDIS_PORT", 6379)),
+                )
+            ],
         },
     }
 }
