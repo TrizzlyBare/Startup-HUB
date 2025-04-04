491837415da56943827960a4fff0bf7f3aa3ebb2
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2025-04-04 07:45:54 +0700

    Update views.py

--- server/myapp/views.py
+++ server/myapp/views.py
@@ -111,15 +111,25 @@ class StartupIdeaViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["get"])
     def my_ideas(self, request):
         """
+        Get current user's ideas (deprecated - use user-ideas instead)
+        """
+        # Just call the new method
+        return self.user_ideas(request)
+
+    @action(detail=False, methods=["get"], url_path="user-ideas")
+    def user_ideas(self, request):
+        """
         Get all startup ideas for a specific user by username.
         If no username is provided, returns the current user's ideas.
         """
         username = request.query_params.get("username")
 
         if username:
+            # Find user by username
             user = get_object_or_404(User, username=username)
             ideas = StartupIdea.objects.filter(user=user)
         else:
+            # If no username provided, default to current user's ideas
             ideas = StartupIdea.objects.filter(user=request.user)
 
         serializer = self.get_serializer(ideas, many=True)
